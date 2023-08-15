96021ecdaa2f6a4a769ad3ed43f201d29776fc53
aaidiv599 <abdulazeezisah69@gmail.com>
2023-08-15 15:20:58 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print _alphabet - print alphabets
+ *
+ */
+void print_alphabet(void);
+{
+	char al;
+	al = 'a';
+	while (al <= 'z')
+	{
+		_putchar('al');
+		al++;
+	}
+	_putchar('\n');
+}
