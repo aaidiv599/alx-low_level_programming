86326b98d6245a9952b6cc59f0b4db2f5205744a
aaidiv599 <abdulazeezisah69@gmail.com>
2023-08-16 19:31:12 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,6 +9,8 @@
  */
 int print_sign(int n)
 {
+	int negative = -1;
+	char ch = (char) negative;
 	if (n > 0)
 	{
 		_putchar('+');
@@ -22,6 +24,6 @@ int print_sign(int n)
 	else
 	{
 		_putchar('-');
-		return (48);
+		return (ch);
 	}
 }
