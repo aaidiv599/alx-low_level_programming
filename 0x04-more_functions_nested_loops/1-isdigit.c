b4baaf33b2d9467f3bf061805cf4f00ca5caffd2
aaidiv599 <abdulazeezisah69@gmail.com>
2023-08-20 06:26:07 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - check if parameter is  a digits or character
+ *
+ * @c: parameter
+ *
+ * Return: 1 if its a number and 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= 10)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
