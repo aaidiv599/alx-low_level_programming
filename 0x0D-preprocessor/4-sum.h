662876d6b6d8419d6cd86d6bfb704e6fb87e0d15
aaidiv599 <abdulazeezisah69@gmail.com>
2023-09-11 11:20:05 -0700

    4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,10 @@
+#ifndef MACRO_H
+#define MACRO_H
+/**
+ * File: 4-sum.h
+ * @x: first value of integer
+ * @y: second value of integer
+ * Desc: sum up x and y
+ */
+#define SUM(x ,y) (x + y)
+#endif
